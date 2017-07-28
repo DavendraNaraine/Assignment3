01ec66ef74775f946b5d5d349de9b820f7961b4a
DevinNived <davendra@intellectstorm.com>
2017-07-27 21:52:54 -0400

    Removed haskell code from Question 1

--- Add Element To List.hs
+++ Add Element To List.hs
@@ -6,27 +6,7 @@
 import Data.List
 import Data.Char
 
-myList = [1,2,3,4,5,6,7,8,9]
 
-firstPart x = take x myList
-secondPart x = drop x myList
-ifFirst x = x : myList
-
-addElement = do
-            print myList
-
-            putStrLn "Enter the number you want to put in the array:"
-            getNumber <- getLine
-            let number = (read getNumber :: Int)
-
-            putStrLn "Enter the position you want the number to be in:"
-            getPosition <- getLine
-            let position = (read getPosition :: Int)
-
-            if position == 1 then ifFirst position
-            else "fkjn"
-
-            print myList  
 
 
 
